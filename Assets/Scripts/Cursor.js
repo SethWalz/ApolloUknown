7852598ddeb44e53c4b0540106152f9e8c0ae20d
tgaskey <tylergaskey@yahoo.com>
2016-04-05 23:24:41 -0500

    Cave Sounds

--- Assets/Scripts/Cursor.js
+++ Assets/Scripts/Cursor.js
@@ -9,6 +9,16 @@
      var load : boolean = false;
      var distance : int = 2;
      
+     var picked: GameObject = null;
+     var speed : int;
+     var friction : float;
+     var lerpSpeed : float;
+     private var xDeg : float;
+     private var yDeg : float;
+     private var zDeg : float;
+     private var fromRotation : Quaternion;
+     private var toRotation : Quaternion;
+     
      function Start()
      {
          Screen.showCursor = false;
@@ -40,7 +50,27 @@
 			 	else if(hit.collider.name == "LoadCave"){
 			 		Application.LoadLevel("cave");
 			 	}
+			 	if(hit.collider.gameObject.tag == "DynamicObject"){
+			 		picked = hit.collider.gameObject;
+			 		xDeg -= Input.GetAxis("Mouse X") * speed * friction;
+			 		yDeg += Input.GetAxis("Mouse Y") * speed * friction;
+			 		zDeg += Input.GetAxis("Mouse Y") * speed * friction;
+			 		fromRotation = picked.transform.parent.rotation;
+			 		toRotation = Quaternion.Euler(0,xDeg,0);
+			 		picked.transform.parent.rotation = Quaternion.Lerp(fromRotation, toRotation, Time.deltaTime * lerpSpeed);
+			 		
+			 		
+			 		//picked = hit.collider.gameObject;
+			 		//picked.transform.rotation = Quaternion.AngleAxis(180, mouse);
+			 		//hit.collider.gameObject.transform.parent=transform; //attach the object to the camera so it moves along with it.
+			 		//hit.collider.gameObject.transform.position=transform.position+transform.forward; //might need changing as it's untested
+			 	}
+			 	
 			}
+		}
+		else if(picked != null){
+			//picked.transform.parent=null;
+			picked = null;
 		}
      }
      
