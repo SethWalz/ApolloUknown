985e1732d0ed7be7adc27d9b9aa6ff612cfdc4dd
Swalz0516 <Swalz0516@gmail.com>
2016-04-18 09:02:52 -0500

    Pause script

--- Assets/Rocks Pack/Standard Assets/Image Effects (Pro Only)/ESCMenu.js
+++ Assets/Rocks Pack/Standard Assets/Image Effects (Pro Only)/ESCMenu.js
@@ -1,19 +1,31 @@
 ﻿#pragma strict
 
-function Start () {
-
-}
+private var pauseGame : boolean = false;
+private var showGUI : boolean = false;
 
 function Update () {
-	 if(Input.GetKey(KeyCode.Escape))
-	 {
-	 	if(Time.timeScale == 0)
-	 	{
-	 		Time.timeScale = 1;
-	 	}
-	 	else
-	 	{
-	 		Time.timeScale = 0;
-	 	}
+	if(Input.GetKeyDown("escape"))
+	{
+		pauseGame = !pauseGame;
+		
+		if(pauseGame == true)
+		 {
+			Time.timeScale = 0;
+			pauseGame = true;
+			GameObject.Find("Main Camera").GetComponent(MouseLook).enabled = false;
+			GameObject.Find("First Person Controller").GetComponent(MouseLook).enabled = false;
+			showGUI = true;
+		 }
 	 }
+	 
+	 if(pauseGame == false)
+	 {
+	 	Time.timeScale = 1;
+	 	pauseGame = false;
+		GameObject.Find("Main Camera").GetComponent(MouseLook).enabled = true;
+		GameObject.Find("First Person Controller").GetComponent(MouseLook).enabled = true;
+		showGUI = false;
+	 }
+	 
+	 
 }
