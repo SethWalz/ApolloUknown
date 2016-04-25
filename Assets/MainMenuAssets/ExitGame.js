e5b4d59d141d176808eba4f52cdb79e26abbad70
Swalz0516 <Swalz0516@gmail.com>
2016-04-25 15:19:00 -0500

    MainMenu

---
+++ Assets/MainMenuAssets/ExitGame.js
@@ -0,0 +1,6 @@
+﻿#pragma strict
+
+
+ function ExitGame(){
+ 	Application.Quit();
+ }
---
+++ Assets/MainMenuAssets/PlayGame.js
@@ -0,0 +1,7 @@
+﻿#pragma strict
+
+public var Level = "0";
+ 
+ function LoadLevel(){
+ Application.LoadLevel("crashLanding");
+ }
